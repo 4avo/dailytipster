d8593e2b58e5d98c668bf5d45c90eb99368bcf07
Chavdar Tsvetkov <tsve0001@hz.nl>
2024-06-24 01:39:44 +0200

    added laravel breeze

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DailyTipster</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png')}}">
+</head>
+<body class="bg-gray-800">
+    <header class="bg-gray-800 text-white p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10 shadow-md">
+        <h1 class="text-4xl font-extrabold tracking-wide animated-header flex items-center space-x-2">   
+            <span class="text-yellow-400 md:text-4xl">RACK$</span>
+            <span class="text-yellow-400"> IN </span>
+            <span class="text-yellow-400">BAGS</span>
+        </h1>
+        <div class="flex items-center">
+            <nav>
+                @auth
+                <a href="/register" class="text-lg md:text-xl text-yellow-400 hover:text-white border-2 border-yellow-400 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-yellow-400">Profile</a>
+                @else
+                <a href="/register" class="text-lg md:text-xl text-yellow-400 hover:text-white border-2 border-yellow-400 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-yellow-400">Join us now</a>
+                @endauth
+            </nav>
+        </div>
+    </header>
+    {{ $slot }}
+    <footer class="bg-gray-800 text-white p-4 mt-6 text-center">
+        <p>&copy; 2024 Our Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,58 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DailyTipster</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="icon" type="image/png" href="{{ asset('images/logo.png')}}">
-</head>
-<body class="bg-gray-800">
-    <header class="bg-gray-800 text-white p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10 shadow-md">
-        <h1 class="text-4xl font-extrabold tracking-wide animated-header flex items-center space-x-2">   
-            <span class="text-yellow-400 md:text-4xl">RACK$</span>
-            <span class="text-yellow-400"> IN </span>
-            <span class="text-yellow-400">BAGS</span>
-        </h1>
-        <div class="flex items-center">
-            <nav>
-                <a href="/join" class="text-lg md:text-xl text-yellow-400 hover:text-white border-2 border-yellow-400 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-yellow-400">Join us now</a>
-            </nav>
-        </div>
-    </header>
-
-    <main class="mt-24 flex"> <!-- Adjust the top margin to account for the fixed header -->
+<x-main>
+    <div class="flex items-center">
+        <nav>
+            @auth
+            <a href="/register" class="text-lg md:text-xl text-yellow-400 hover:text-white border-2 border-yellow-400 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-yellow-400">You are logged in</a>
+            @else
+            <a href="/register" class="text-lg md:text-xl text-yellow-400 hover:text-white border-2 border-yellow-400 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-yellow-400">Join us now</a>
+            @endauth
+        </nav>
+    </div>
+ <div class="mt-24 flex"> <!-- Adjust the top margin to account for the fixed header -->
         <div class="w-1/4 px-4">
             <h2 class="text-white text-xl mb-4">Our top predictors</h2>
-            <div class="leaderboard-item">
-                <span class="rank">1.</span>
-                <span class="username">Username 1</span>
-            </div>
-            <div class="leaderboard-item">
-                <span class="rank">2.</span>
-                <span class="username">Username 2</span>
-            </div>
-            <div class="leaderboard-item">
-                <span class="rank">3.</span>
-                <span class="username">Username 3</span>
-            </div>
+            @foreach ($users as $user)
+                <div class="leaderboard-item left">
+                    <span class="rank">{{ $loop->iteration }}.</span>
+                    <span class="username">{{ $user->name }}</span>
+                </div>
+            @endforeach
         </div>
 
         <section class="main-content w-1/2 px-4">
-            <div class="feature golden-border mb-4">
+            <div class="feature mb-4">
                 <h2>Compete and win</h2>
                 <p>Our website is a challenge for every football fan. If you want to compete with others and win exclusive rewards, join us now and start predicting the outcomes of the upcoming matches.</p>
                 <img src="{{ asset('images/compete.png') }}" alt="Profile pic" class="w-full h-full object-cover">
             </div>
 
-            <div class="feature golden-border mb-4">
+            <div class="feature mb-4">
                 <h2>Our Mission</h2>
                 <p>Our application allows users to compete against each other by predicting football match outcomes. We've created a platform that offers a safe and enjoyable alternative to traditional betting, encouraging sports enthusiasts to test their prediction skills in a friendly, competitive environment. By promoting strategic thinking and sports knowledge, we aim to help users make informed predictions and foster a community of passionate football fans.</p>
                 <img src="{{ asset('images/leagues.png') }}" alt="Profile pic" class="w-full h-full object-cover">
             </div>
 
-            <div class="feature golden-border mb-4">
+            <div class="feature mb-4">
                 <h2>Join Our Community</h2>
                 <p>Explore our curated collection and join a community of fashion enthusiasts who appreciate the artistry behind each piece.</p>
                 <a href="/join" class="text-lg md:text-xl text-yellow-400 hover:text-white border-2 border-yellow-400 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-yellow-400 mt-4 inline-block">Start Exploring</a>
@@ -75,9 +55,5 @@
             </div>
         </div>
     </main>
-
-    <footer class="bg-gray-800 text-white p-4 mt-6 text-center">
-        <p>&copy; 2024 Our Website. All rights reserved.</p>
-    </footer>
-</body>
-</html>
+ </div>
+</x-main>
